19ad0be7344e09c0c3c0ea95aa383a0a530ea27b
WeiZhiYuan <weizy@embest-tech.(none)>
2016-02-18 13:33:56 +0800

    wifi 3G audio all ok,this is the first release version for customer

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 142 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#142 Fri Jan 15 17:14:24 CST 2016"
+#define LINUX_COMPILE_BY "weizy"
+#define LINUX_COMPILE_HOST "embest-tech"
+#define LINUX_COMPILER "gcc version 4.3.3 (Sourcery G++ Lite 2009q1-203) "
